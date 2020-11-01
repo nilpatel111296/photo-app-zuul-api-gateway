51e21c3aa35ce8b42c379b75d23cc9819d1cd832
Nil <nilpatel111296@gmail.com>
2020-11-01 00:42:57 -0500

    Initial Commit

---
+++ src/main/java/com/photoapp/api/gateway/photoappzuulapigateway/PhotoAppZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.photoapp.api.gateway.photoappzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppZuulApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppZuulApiGatewayApplication.class, args);
+    }
+
+}
